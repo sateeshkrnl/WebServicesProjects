bff8f3a205d308588e9e5481879e85911295bb56
Sateesh Kumar V <sateeshkrnl@gmail.com>
2016-03-22 17:38:52 +0530

    initial project setup

---
+++ TestJerseyWeb/src/main/java/com/test/jersey/Adage.java
@@ -0,0 +1,33 @@
+package com.test.jersey;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "adage")
+public class Adage {
+	private String words;
+	private int wordCount;
+
+	public Adage() {
+	}
+
+	@Override
+	public String toString() {
+		return words + " -- " + wordCount + " words";
+	}
+
+	public void setWords(String words) {
+		this.words = words;
+		this.wordCount = words.trim().split("\\s+").length;
+	}
+
+	public String getWords() {
+		return this.words;
+	}
+
+	public void setWordCount(int wordCount) {
+		this.wordCount = wordCount;
+	}
+
+	public int getWordCount() {
+		return this.wordCount;
+	}
+}
---
+++ TestJerseyWeb/src/main/java/com/test/jersey/RestfulAdage.java
@@ -0,0 +1,16 @@
+package com.test.jersey;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/resourcesA")
+public class RestfulAdage extends Application {
+	@Override
+	public Set<Class<?>> getClasses() {
+		Set<Class<?>> set = new HashSet<Class<?>>();
+		set.add(Adages.class);
+		return set;
+	}
+}
